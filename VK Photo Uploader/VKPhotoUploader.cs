8533b272812dbf8f3ae7fa79b5225a8239c51e42
alikhil <alikhil@mail.ru>
2015-07-07 03:22:35 +0500

    Добавлена фунция загрузки множества фотографий

--- VK Photo Uploader/VKPhotoUploader.cs
+++ VK Photo Uploader/VKPhotoUploader.cs
@@ -10,6 +10,10 @@ using Newtonsoft.Json;
 using System.Net;
 using System.IO;
 
+using VkNet.Exception;
+
+using VK_Photo_Uploader.Classes;
+
 namespace VK_Photo_Uploader
 {
     public static class VKPhotoUploader
@@ -29,6 +33,41 @@ namespace VK_Photo_Uploader
             }
         }
 
+        public static async Task<string> UploadImages(long ownerId, string [] files, string message)
+        {
+            string result = "OK";
+            try
+            {
+                var serverUp = Api.Photo.GetWallUploadServer(ownerId);
+                var url = serverUp.UploadUrl;
+
+                var coll = new List<VkNet.Model.Attachments.Photo>().AsEnumerable();
+                foreach (var file in files)
+                {
+                    var res = await UploadFile(url, file);
+                    var response = JsonConvert.DeserializeObject<PhotoResponse>(res);
+                    var col = Api.Photo.SaveWallPhoto(response.Photo, Api.UserId, ownerId, response.Server, response.Hash);
+                    coll = coll.Concat(col);
+                }
+                long type = 1;
+                var owner = Api.Utils.ResolveScreenName("public" + ownerId);
+                if (owner.Id == ownerId)
+                    type = -1;
+                var post = Api.Wall.Post(ownerId * type, false, true, message, coll, signed:true);
+            }
+            catch (AccessDeniedException e)
+            {
+                result = "Доступ запрещен!";
+            }
+            catch (Exception e)
+            {
+                if(result == "OK")
+                    result = "Не удалось загрузить...";
+            }
+           
+            return result;
+        }
+
         private static async Task<string> UploadFile(string url, string fName)
         {
             WebClient wClient = new WebClient();
@@ -36,5 +75,6 @@ namespace VK_Photo_Uploader
             string res = System.Text.Encoding.Default.GetString(ans);
             return res;
         }
+
     }
 }
