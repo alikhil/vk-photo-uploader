d37ed41d02c51a238d8206ffee623000e4dc74f6
alikhil <alikhil@mail.ru>
2015-07-07 00:22:20 +0500

    Добавлен класс результата первичной загрузки фото - PhotoResponse

---
+++ VK Photo Uploader/Classes/PhotoResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VK_Photo_Uploader.Classes
+{
+    public class PhotoResponse
+    {
+        public long Server { get; set; }
+        public string Photo { get; set; }
+        public string Hash { get; set; }
+    }
+}
